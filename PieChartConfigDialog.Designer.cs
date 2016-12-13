96b8ddba2eaec442e513522dce3500ddf5898227
toehead2001 <toe_head2001@yahoo.com>
2016-12-13 15:55:33 -0800

    Switch to HiDPI scaling

--- PieChartConfigDialog.Designer.cs
+++ PieChartConfigDialog.Designer.cs
@@ -448,8 +448,8 @@
             // PieChartConfigDialog
             // 
             this.AcceptButton = this.btnOK;
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
             this.BackColor = System.Drawing.SystemColors.Window;
             this.CancelButton = this.btnCancel;
             this.ClientSize = new System.Drawing.Size(284, 482);
