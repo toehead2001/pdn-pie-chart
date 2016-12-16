b9068a5e679ceb7656286add282544d75da0ce11
toehead2001 <toe_head2001@yahoo.com>
2016-12-15 21:03:47 -0800

    Remove FileName initializer

--- PieChartConfigDialog.Designer.cs
+++ PieChartConfigDialog.Designer.cs
@@ -369,7 +369,6 @@
             // 
             // openFileDialog1
             // 
-            this.openFileDialog1.FileName = "openFileDialog1";
             this.openFileDialog1.Filter = "CSV (*.csv)|*.csv";
             // 
             // PieChartConfigDialog
