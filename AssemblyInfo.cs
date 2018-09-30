64a32d263befea2aaabe05b3fbb853779ab0f54c
toe_head2001 <toe_head2001@yahoo.com>
2018-09-29 18:02:08 -0600

    Update assembly info

--- AssemblyInfo.cs
+++ AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("Chart")]
 [assembly: AssemblyCompany("toe_head2001")]
 [assembly: AssemblyProduct("Pie Chart")]
-[assembly: AssemblyCopyright("Copyright © 2016 toe_head2001")]
+[assembly: AssemblyCopyright("Copyright © 2018 toe_head2001")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
